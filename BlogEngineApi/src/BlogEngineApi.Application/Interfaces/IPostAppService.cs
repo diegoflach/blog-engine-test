b9bfdee97edb8b6c05a40a7b6bad93898085305f
Diego Flach <dflach@opsolve.com>
2023-09-11 21:08:35 -0300

    IPostAppService added

---
+++ BlogEngineApi/src/BlogEngineApi.Application/Interfaces/IPostAppService.cs
@@ -0,0 +1,17 @@
+﻿using BlogEngineApi.Domain.Dto;
+using BlogEngineApi.Domain.Dtos;
+using BlogEngineApi.Domain.Dtos.Result;
+using BlogEngineApi.Domain.Dtos.Set;
+
+namespace BlogEngineApi.Application.Interfaces
+{
+    public interface IPostAppService
+    {
+        PostsResultDto GetAll(Guid? author = null);
+        PostDto Set(SetPostDto postDto);
+        void Submit(Guid postId);
+        void Approve(Guid postId);
+        void Reject(PostDto postDto);
+        PostDto Comment(CommentDto commentDto);
+    }
+}
