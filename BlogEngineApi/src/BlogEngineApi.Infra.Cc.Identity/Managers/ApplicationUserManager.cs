9d60ca454df115333fe88c42f5cf7dcac1099ca0
Diego Flach <dflach@opsolve.com>
2023-09-11 21:06:55 -0300

    ApplicationUserManager added to Identity

---
+++ BlogEngineApi/src/BlogEngineApi.Infra.Cc.Identity/Managers/ApplicationUserManager.cs
@@ -0,0 +1,30 @@
+﻿using BlogEngineApi.Infra.Cc.Identity.Context;
+using BlogEngineApi.Infra.Cc.Identity.Models;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace BlogEngineApi.Infra.Cc.Identity.Managers
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        private readonly IdentityApplicationDbContext dbContext;
+
+        public ApplicationUserManager(IUserStore<ApplicationUser> store, IOptions<IdentityOptions> optionsAccessor,
+            IPasswordHasher<ApplicationUser> passwordHasher, IEnumerable<IUserValidator<ApplicationUser>> userValidators,
+            IEnumerable<IPasswordValidator<ApplicationUser>> passwordValidators, ILookupNormalizer keyNormalizer,
+            IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<ApplicationUser>> logger, 
+            IdentityApplicationDbContext dbContext): base(store, optionsAccessor,passwordHasher,
+            userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+        { 
+            this.dbContext = dbContext;
+        }
+
+        public ApplicationUser GetByEmail(string email)
+        {
+            return dbContext.ApplicationUsers.Include(u => u.Roles).FirstOrDefault(u => u.Email == email);
+        }
+    }
+}
--- BlogEngineApi/src/BlogEngineApi.Infra.Cc.Identity/Models/ApplicationUser.cs
+++ BlogEngineApi/src/BlogEngineApi.Infra.Cc.Identity/Models/ApplicationUser.cs
@@ -4,5 +4,6 @@ namespace BlogEngineApi.Infra.Cc.Identity.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public virtual ICollection<IdentityUserRole<Guid>> Roles { get; } = new List<IdentityUserRole<Guid>>();
     }
 }
