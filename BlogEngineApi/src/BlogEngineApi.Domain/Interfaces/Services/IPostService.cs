82aecf04be5b15c558ad011c8dcc24c32c9daa49
Diego Flach <dflach@opsolve.com>
2023-09-11 21:06:27 -0300

    Domain objects clean up

--- BlogEngineApi/src/BlogEngineApi.Domain/Interfaces/Services/IPostService.cs
+++ BlogEngineApi/src/BlogEngineApi.Domain/Interfaces/Services/IPostService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlogEngineApi.Domain.Interfaces.Services
+﻿namespace BlogEngineApi.Domain.Interfaces.Services
 {
     internal interface IPostService
     {
